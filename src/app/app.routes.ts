301b32aaaed2914568e09d53d736e907a379dc39
Jose Carlos <suportejr@outlook.com>
2024-05-24 15:51:06 -0300

    feat: adiciona página de edição

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,4 +14,11 @@ export const routes: Routes = [
                 (m) => m.CreateComponent
             ),
     },
+    {
+        path: 'edit-product',
+        loadComponent: ()=>
+            import('./features/edit/edit.component').then(
+                (m) => m.EditComponent
+            ),
+    },
 ];
